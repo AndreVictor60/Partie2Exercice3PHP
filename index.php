8e0a15537ac9c01ea5a48c8dffbf8f57496a97fe
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 11:04:13 +0100

    fini exercice3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  $age = 22;
+  $genre = 'homme';
+  if($age > 18 && $genre=='homme'){
+    echo 'Vous êtes un homme et vous êtes majeur';
+  }elseif ($age < 18 && $genre=='homme') {
+    echo 'Vous êtes un homme et vous êtes mineur';
+  }elseif($age > 18 && $genre=='femme'  ) {
+    echo 'Vous êtes une femme et vous êtes majeur';
+  }elseif($age < 18 && $genre=='femme'){
+    echo 'Vous êtes une femme et vous êtes mineur';
+  }else{
+    echo 'error';
+  }
+ ?>
